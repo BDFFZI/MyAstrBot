75b2d0d5ea8b17f073a185f7fefe3a32dd2b8ee9
BDFFZI <1330958515@qq.com>
2026-02-27 12:22:36 +0800

    -

--- main.py
+++ main.py
@@ -2,23 +2,31 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+
+@register("MyAstrBot", "BDFFZI", "BDFFZI的AstrBot", "1.0.0")
 class MyPlugin(Star):
+    # 系统构造函数
     def __init__(self, context: Context):
         super().__init__(context)
 
+    # 插件开始事件
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        pass
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
+    # 插件销毁事件
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        pass
+
+    # 全局信息过滤器
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def on_all_message(self, event: AstrMessageEvent):
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result("收到了一条消息。")
+
+    # /hello 指令
+    @filter.command("hello")
+    async def helloworld(self, event: AstrMessageEvent):
+        """这是一个 hello 指令"""  # 指令描述
+        user_name = event.get_sender_name()
+        yield event.plain_result(f"你好，{user_name}！")
